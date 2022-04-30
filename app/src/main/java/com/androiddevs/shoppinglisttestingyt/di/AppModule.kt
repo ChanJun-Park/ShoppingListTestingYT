25a35d259b6d69b8c91d7bae7a2d93e5cb7168ec
박찬준 <pcj01142@gmail.com>
2022-04-30 20:07:31 +0900

    fake repository

---
+++ app/src/main/java/com/androiddevs/shoppinglisttestingyt/di/AppModule.kt
@@ -0,0 +1,47 @@
+package com.androiddevs.shoppinglisttestingyt.di
+
+import android.content.Context
+import androidx.room.Room
+import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingDao
+import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingItemDatabase
+import com.androiddevs.shoppinglisttestingyt.data.remote.PixabayAPI
+import com.androiddevs.shoppinglisttestingyt.other.Constants.BASE_URL
+import com.androiddevs.shoppinglisttestingyt.other.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideShoppingItemDatabase(@ApplicationContext context: Context): ShoppingItemDatabase {
+        return Room
+            .databaseBuilder(context, ShoppingItemDatabase::class.java, DATABASE_NAME)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideShoppingDao(shoppingItemDatabase: ShoppingItemDatabase): ShoppingDao {
+        return shoppingItemDatabase.shoppingDao()
+    }
+
+    @Provides
+    @Singleton
+    fun providePixabayApi(): PixabayAPI {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PixabayAPI::class.java)
+    }
+
+}
