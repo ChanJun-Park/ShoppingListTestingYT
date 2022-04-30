3097d21dfc07f0a6e3ec4bfdd61ef37cf9942020
박찬준 <pcj01142@gmail.com>
2022-04-30 21:37:20 +0900

    test commit

--- app/src/main/java/com/androiddevs/shoppinglisttestingyt/MyApplication.kt
+++ app/src/main/java/com/androiddevs/shoppinglisttestingyt/MyApplication.kt
@@ -3,5 +3,6 @@ package com.androiddevs.shoppinglisttestingyt
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+// test
 @HiltAndroidApp
 class MyApplication : Application()
